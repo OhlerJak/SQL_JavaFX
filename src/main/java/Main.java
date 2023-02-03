1874bac600fbd8cfbe03be0a7c70586771abd8ef
SchopperAle <alexander.schopper@htl-wels.at>
2023-02-03 08:57:38 +0100

    Dings

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,20 @@
 import javafx.application.Application;
 import javafx.stage.Stage;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Main extends Application {
+
+    public static Connection c;
+
+
+    public static void main(String[] args) throws SQLException {
+        c = DriverManager.getConnection("jdbc:derby://localhost:1527/CooleDB","app","password");
+
+        launch(args);
+    }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
