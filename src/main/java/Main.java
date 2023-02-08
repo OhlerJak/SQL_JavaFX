4b1be105b0545f130547ec66f43e3fff31c5fad7
SchopperAle <alexander.schopper@htl-wels.at>
2023-02-08 15:33:29 +0100

    Imports weg

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,11 +2,6 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 import view.PersonController;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
-
 public class Main extends Application {
     public static void main(String[] args) {
         launch(args);    
